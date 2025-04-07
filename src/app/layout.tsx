ac6acc305a94bcc752c754590bc7658a40bd22b6
Majd Hemud <majd.hemud@gmail.com>
2025-04-07 13:37:55 -0500

    lint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import { homePath, ticketsPath } from "@/paths";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
+import { homePath, ticketsPath } from "@/paths";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",

6a79d1b9ed7df9f3b5f01430d82dbad4973fe049
Majd Hemud <majd.hemud@gmail.com>
2025-04-07 20:15:13 -0500

    icons + theming

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,3 +1,4 @@
+import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react";
 import Link from "next/link";
 import {
 	Card,
@@ -10,48 +11,10 @@ import { Separator } from "@/components/ui/separator";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
-const DocumentIcon = () => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 24 24"
-		fill="currentColor"
-		className="size-6"
-	>
-		<path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
-		<path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
-	</svg>
-);
-
-const PencilIcon = () => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 24 24"
-		fill="currentColor"
-		className="size-6"
-	>
-		<path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
-	</svg>
-);
-
-const CheckIcon = () => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 24 24"
-		fill="currentColor"
-		className="size-6"
-	>
-		<path
-			fillRule="evenodd"
-			d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-			clipRule="evenodd"
-		/>
-	</svg>
-);
-
 const TICKET_ICONS = {
-	OPEN: <DocumentIcon />,
-	IN_PROGRESS: <PencilIcon />,
-	DONE: <CheckIcon />,
+	OPEN: <LucideFileText />,
+	DONE: <LucideCheckCircle />,
+	IN_PROGRESS: <LucidePencil />,
 };
 
 const TicketsPage = () => {

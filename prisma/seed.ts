6fd76928a32572424a655e7f36d942e7f2ec03f6
Majd Hemud <majd.hemud@gmail.com>
2025-05-03 10:11:29 -0500

    remove error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,32 +2,32 @@ import { PrismaClient } from "@/generated/prisma";
 
 const prisma = new PrismaClient();
 
-const tickets=[
-    {
-        title:"Ticket 1",
-        content:"This is the first ticket from DB",
-        status:"DONE" as const,
-    },
-    {
-        title:"Ticket 2",
-        content:"This is the second ticket from DB",
-        status:"OPEN" as const,
-    },
-    {
-        title:"Ticket 3",
-        content:"This is the third ticket from DB",
-        status:"IN_PROGRESS" as const,
-    },
+const tickets = [
+  {
+    title: "Ticket 1",
+    content: "This is the first ticket from DB",
+    status: "DONE" as const,
+  },
+  {
+    title: "Ticket 2",
+    content: "This is the second ticket from DB",
+    status: "OPEN" as const,
+  },
+  {
+    title: "Ticket 3",
+    content: "This is the third ticket from DB",
+    status: "IN_PROGRESS" as const,
+  },
 ];
 
-const seed=async()=>{
-    const t0=performance.now();
-    console.log
-    await prisma.ticket.deleteMany();
+const seed = async () => {
+  // const t0=performance.now();
+  // console.log
+  await prisma.ticket.deleteMany();
 
-    await prisma.ticket.createMany({
-        data:tickets,
-    })
+  await prisma.ticket.createMany({
+    data: tickets,
+  });
 };
 
 seed();

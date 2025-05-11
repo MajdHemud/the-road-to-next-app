fe4e65732309625a2625b7438a19b3ac3f06a3b9
Ruwaid sami abu hmoud <toxicinteger@gmail.com>
2025-05-11 16:33:14 -0500

    code update 10.5 -2

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -5,17 +5,19 @@ import { Placeholder } from "@/components/placeholder";
 import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
+//export const revalidate = 30;
+
 const TicketsPage = () => {
-	return (
-		<div className="flex-1 flex flex-col gap-y-8">
-			<Heading title="Tickets" description="All your tickets at one place" />
-			<ErrorBoundary fallback={<Placeholder label="Something went wrong" />}>
-				<Suspense fallback={<Spinner />}>
-					<TicketList />
-				</Suspense>
-			</ErrorBoundary>
-		</div>
-	);
+  return (
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Heading title="Tickets" description="All your tickets at one place" />
+      <ErrorBoundary fallback={<Placeholder label="Something went wrong" />}>
+        <Suspense fallback={<Spinner />}>
+          <TicketList />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  );
 };
 
 export default TicketsPage;

202da0ed49d98f9cb569892baa50796561d7cea1
Ruwaid sami abu hmoud <toxicinteger@gmail.com>
2025-05-21 12:04:59 -0500

    code update 21-5 (2)

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,16 +1,10 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { CardCompact } from "@/components/card-compact";
 import { Heading } from "@/components/heading";
 import { Placeholder } from "@/components/placeholder";
 import { Spinner } from "@/components/spinner";
 import { TicketCreateForm } from "@/components/ticket-create-form";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
 //export const revalidate = 30;
@@ -19,15 +13,12 @@ const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
-      <Card className="w-full max-w-[420px] self-center">
-        <CardHeader>
-          <CardTitle>Create Ticket</CardTitle>
-          <CardDescription>A new ticket will be created</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <TicketCreateForm />
-        </CardContent>
-      </Card>
+      <CardCompact
+        title="Create Ticket"
+        description="A new ticket will be created"
+        className="w-full max-w-[420px] self-center"
+        content={<TicketCreateForm />}
+      />
       <ErrorBoundary fallback={<Placeholder label="Something went wrong" />}>
         <Suspense fallback={<Spinner />}>
           <TicketList />
--- src/components/ticket-create-form.tsx
+++ src/components/ticket-create-form.tsx
@@ -1,3 +1,6 @@
+import { revalidatePath } from "next/cache";
+import { prisma } from "@/lib/prisma";
+import { ticketsPath } from "@/paths";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
@@ -11,7 +14,16 @@ const TicketCreateForm = () => {
       title: formData.get("title"),
       content: formData.get("content"),
     };
-    console.log(data);
+
+    await prisma.ticket.create({
+      data: {
+        title: data.title as string,
+        content: data.content as string,
+      },
+    });
+
+    // Redirect to the tickets page after creating a ticket
+    revalidatePath(ticketsPath());
   };
 
   return (

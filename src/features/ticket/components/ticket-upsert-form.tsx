99e99007fd9be63c1442af67a58a8dc7df847147
Majd Hemud <majd.hemud@gmail.com>
2025-06-06 08:23:54 -0500

    code update 6/6

---
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { LucideLoaderCircle } from "lucide-react";
+import { useActionState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Ticket } from "@/generated/prisma";
+import { upsertTicket } from "../actions/upsert-ticket";
+
+type TicketUpsertFormProps = {
+  ticket?: Ticket;
+};
+
+const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
+  const [actionState, action, isPending] = useActionState(
+    upsertTicket.bind(null, ticket?.id),
+    { message: "" }
+  );
+
+  return (
+    <form action={action} className="flex flex-col gap-y-2">
+      <Label htmlFor="title">Title</Label>
+      <Input id="title" name="title" type="text" defaultValue={ticket?.title} />
+
+      <Label htmlFor="content">Content</Label>
+      <Textarea id="content" name="content" defaultValue={ticket?.content} />
+      <Button disabled={isPending} type="submit">
+        {isPending && <LucideLoaderCircle className="w-4 h-4 animate-spin" />}
+        {ticket ? "Edit" : "Create"}
+      </Button>
+      {actionState.message}
+    </form>
+  );
+};
+
+export { TicketUpsertForm };

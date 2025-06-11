ace1ac1dad2c58a36d54743cd610df19bd0f4ac0
Majd Hemud <majd.hemud@gmail.com>
2025-06-11 08:35:59 -0500

    code update 11/6 - 1

---
+++ src/features/ticket/components/ticket-more-menu.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { LucideTrash } from "lucide-react";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Ticket, TicketStatus } from "@/generated/prisma";
+import { TICKET_STATUS_LABELS } from "../constants";
+
+type TicketMoreMenuProps = {
+  ticket: Ticket;
+  trigger: React.ReactElement;
+};
+
+const TicketMoreMenu = ({ ticket, trigger }: TicketMoreMenuProps) => {
+  const deleteButton = (
+    <DropdownMenuItem>
+      <LucideTrash className="h-4 w-4" />
+      <span>Delete</span>
+    </DropdownMenuItem>
+  );
+
+  const handleUpdateTicketStatus = async (value: string) => {
+    // TODO update ticket status
+  };
+
+  const ticketStatusRadioGroupItems = (
+    <DropdownMenuRadioGroup
+      value={ticket.status}
+      onValueChange={handleUpdateTicketStatus}
+    >
+      {(Object.keys(TICKET_STATUS_LABELS) as Array<TicketStatus>).map((key) => (
+        <DropdownMenuRadioItem key={key} value={key}>
+          {TICKET_STATUS_LABELS[key]}
+        </DropdownMenuRadioItem>
+      ))}
+    </DropdownMenuRadioGroup>
+  );
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" side="right">
+        {ticketStatusRadioGroupItems}
+        <DropdownMenuSeparator />
+        {deleteButton}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export { TicketMoreMenu };

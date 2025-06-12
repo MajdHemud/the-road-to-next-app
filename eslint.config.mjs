d616cfc00e2e2b8ac602ac27ce160a5282a15097
Majd Hemud <majd.hemud@gmail.com>
2025-06-12 11:19:26 -0500

    ignore generated/prisma folder for linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,34 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-	...compat.config({
-		extends: ["next/core-web-vitals", "next/typescript"],
-		plugins: ["simple-import-sort"],
-		rules: {
-			"simple-import-sort/imports": [
-				"error",
-				{
-					groups: [["^\\u0000", "^@?\\w", "^[^.]", "^\\."]],
-				},
-			],
-			"simple-import-sort/exports": "error",
-			quotes: ["error", "double"],
-		},
-		parserOptions: {
-			sourceType: "module",
-			ecmaVersion: "latest",
-		},
-	}),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    plugins: ["simple-import-sort"],
+    ignorePatterns: ["src/generated/prisma/**"],
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [["^\\u0000", "^@?\\w", "^[^.]", "^\\."]],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+      quotes: ["error", "double"],
+    },
+    parserOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+    },
+  }),
 ];
 
 export default eslintConfig;
